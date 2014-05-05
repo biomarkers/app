02bd32758ac31c231e3b425d393a33e017d29401
hessk <evokinglegato@gmail.com>
2014-05-05 16:48:40 -0700

    'Add component' UI cleanup

--- App/App/JKKAddComponentViewController.mm
+++ App/App/JKKAddComponentViewController.mm
@@ -13,6 +13,9 @@
 @property NSInteger currentEndTimeMinutes;
 @property NSInteger currentEndTimeSeconds;
 
+@property NSInteger currentStartTimeMinutes;
+@property NSInteger currentStartTimeSeconds;
+
 @end
 
 @implementation JKKAddComponentViewController
@@ -25,6 +28,9 @@
         
         self.currentEndTimeMinutes = 0;
         self.currentEndTimeSeconds = 0;
+        
+        self.currentStartTimeMinutes = 0;
+        self.currentStartTimeSeconds = 0;
     }
     return self;
 }
@@ -71,31 +77,53 @@
 }
 
 - (IBAction)updateRuntime:(id)sender {
-    /* hessk: mixing UI elements and processing here. volatile...? */
+    /* hessk: TODO: clean this up. mixing UI elements and processing here */
+    
+    // handle end stepper wrap
     if (self.currentEndTimeSeconds == [self.secondStepper maximumValue] && [self.secondStepper value] == [self.secondStepper minimumValue]) {
         // positive wrap must've occurred...
         self.minuteStepper.value++;
     }
-    
     if (self.currentEndTimeSeconds == [self.secondStepper minimumValue] && [self.secondStepper value] == [self.secondStepper maximumValue]) {
         // negative wrap must've occurred...
         self.minuteStepper.value--;
     }
     
+    // handle start stepper wrap
+    if (self.currentStartTimeSeconds == [self.startSecondStepper maximumValue] && [self.startSecondStepper value] == [self.startSecondStepper minimumValue]) {
+        // positive wrap must've occurred...
+        self.startMinuteStepper.value++;
+    }
+    if (self.currentStartTimeSeconds == [self.startSecondStepper minimumValue] && [self.startSecondStepper value] == [self.startSecondStepper maximumValue]) {
+        // negative wrap must've occurred...
+        self.startMinuteStepper.value--;
+    }
+    
+    // ensure start time is less than end time
+    if ([self getStartIntervalFromStepper] > [self getEndIntervalFromStepper]) {
+        if (sender == self.startSecondStepper || sender == self.startMinuteStepper) {
+            [self.minuteStepper setValue:[self.startMinuteStepper value]];
+            [self.secondStepper setValue:[self.startSecondStepper value]];
+        } else {
+            [self.startMinuteStepper setValue:[self.minuteStepper value]];
+            [self.startSecondStepper setValue:[self.secondStepper value]];
+        }
+    }
+    
     self.currentEndTimeMinutes = [self.minuteStepper value];
     self.currentEndTimeSeconds = [self.secondStepper value];
     
-    [self updateRuntimeControls];
+    self.currentStartTimeMinutes = [self.startMinuteStepper value];
+    self.currentStartTimeSeconds = [self.startSecondStepper value];
     
-    /* validate negative values for start and endtime */
+    [self updateRuntimeControls];
 }
 
 - (void)updateRuntimeControls {
-    self.minuteLabel.text = [[@(self.currentEndTimeMinutes) stringValue] stringByAppendingString:@" min"];
-    self.secondLabel.text = [[@(self.currentEndTimeSeconds) stringValue] stringByAppendingString:@" sec"];
-    
-    self.startMinuteLabel.text = [[NSString stringWithFormat:@"%.0f", self.startMinuteStepper.value] stringByAppendingString:@" min"];
-    self.startSecondLabel.text = [[NSString stringWithFormat:@"%.0f", self.startSecondStepper.value] stringByAppendingString:@" sec"];
+    self.minuteLabel.text = [NSString stringWithFormat:@"%d min", self.currentEndTimeMinutes];
+    self.secondLabel.text = [NSString stringWithFormat:@"%d sec", self.currentEndTimeSeconds];
+    self.startMinuteLabel.text = [NSString stringWithFormat:@"%d min", self.currentStartTimeMinutes];
+    self.startSecondLabel.text = [NSString stringWithFormat:@"%d sec", self.currentStartTimeSeconds];
     
     // second stepper wrapping dictated by max and min values of minute stepper
     if ((self.currentEndTimeMinutes > [self.minuteStepper minimumValue] || self.currentEndTimeSeconds > [self.secondStepper minimumValue]) && (self.currentEndTimeMinutes < [self.minuteStepper maximumValue] || self.currentEndTimeSeconds < [self.secondStepper maximumValue])) {
@@ -103,19 +131,35 @@
     } else {
         [self.secondStepper setWraps:NO];
     }
+    
+    // same for start time
+    if ((self.currentStartTimeMinutes > [self.startMinuteStepper minimumValue] || self.currentStartTimeSeconds > [self.startSecondStepper minimumValue]) && (self.currentStartTimeMinutes < [self.startMinuteStepper maximumValue] || self.currentStartTimeSeconds < [self.startSecondStepper maximumValue])) {
+        [self.startSecondStepper setWraps:YES];
+    } else {
+        [self.startSecondStepper setWraps:NO];
+    }
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     /* check to see if the user pressed done - if so, set this controllers model component object */
     if (sender != self.doneButton) return;
     
-    NSTimeInterval endTime = self.currentEndTimeMinutes * 60 + self.currentEndTimeSeconds;
-    NSTimeInterval startTime = self.startMinuteStepper.value * 60 + self.startSecondStepper.value;
+    NSTimeInterval endTime = [self getEndIntervalFromStepper];
+    NSTimeInterval startTime = [self getStartIntervalFromStepper];
     
     self.component = [[JKKComponent alloc] initWithVarType:(ModelComponent::VariableType)[self.channelSelector selectedSegmentIndex]
                                                          withModelType:(ModelComponent::ModelType)[self.typeSelector selectedSegmentIndex]
                                                          withStartTime:startTime
                                                          withEndTime:endTime];
+    
+}
+
+- (NSTimeInterval)getStartIntervalFromStepper {
+    return self.startMinuteStepper.value * 60 + self.startSecondStepper.value;
+}
+
+- (NSTimeInterval)getEndIntervalFromStepper {
+    return self.minuteStepper.value * 60 + self.secondStepper.value;
 }
 
 @end
